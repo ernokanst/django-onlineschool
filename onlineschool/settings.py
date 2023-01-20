c207d6deb9f6d68cc62a64218d48c2a8df5067f5
Дмитрий Канский <ernokanst@PowerBook-G6.local>
2023-01-20 14:31:10 +0300

    Pagination

--- onlineschool/settings.py
+++ onlineschool/settings.py
@@ -131,5 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
